1b257f06222458ae7cc39cd8c1dae48b5c0ef618
ysmk620 <0620komako@gmail.com>
2025-12-22 12:22:43 +0900

    Email表示の追加

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -3,9 +3,6 @@ package com.example.jetprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,6 +17,9 @@ import com.example.jetprofile.ui.theme.JetProfileTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 
@@ -87,6 +87,33 @@ class MainActivity : ComponentActivity() {
                                 color=Color.Gray,
                                 fontSize=16.sp,
                             )
+                            Spacer(modifier=Modifier.height(20.dp))
+
+                            //Email
+                            Row(verticalAlignment = Alignment.CenterVertically){
+                                Icon(
+                                    imageVector= Icons.Default.Email,
+                                    contentDescription="email"
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(
+                                    text="Email",
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Bold,
+                                )
+                            }
+                            Spacer(modifier=Modifier.height(10.dp))
+                            Text(
+                                text = "example@example.com",
+                                fontSize = 16.sp,
+                            )
+                            Spacer(modifier=Modifier.height(5.dp))
+
+                            Divider(
+                                thickness = 2.dp,
+                                modifier = Modifier.clip(RoundedCornerShape(1000.dp)),
+                            )
+
                         }
 
                     }
