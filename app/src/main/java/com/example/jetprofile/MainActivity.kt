8bc591e5277489cc03bbd11b31eedd0e78de6181
ysmk620 <0620komako@gmail.com>
2025-12-24 21:48:15 +0900

    趣味＆居住地セクションの追加

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -15,12 +15,17 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetprofile.ui.theme.JetProfileTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 
 
@@ -53,7 +58,7 @@ class MainActivity : ComponentActivity() {
                         //名前
                         Text(
                             text = "山田太郎",
-                            color = Color.Gray,
+                            color = Gray,
                             fontSize = 16.sp,
                             fontWeight = FontWeight.ExtraBold
                         )
@@ -63,7 +68,7 @@ class MainActivity : ComponentActivity() {
                         //職業
                         Text(
                             text ="職業：Androidエンジニア",
-                            color = Color.Gray,
+                            color = Gray,
                             fontSize = 16.sp,
                             fontWeight = FontWeight.ExtraBold
                         )
@@ -84,24 +89,14 @@ class MainActivity : ComponentActivity() {
                             //部署・グループ
                             Text(
                                 text="DXカンパニー　テクノロジーグループ",
-                                color=Color.Gray,
+                                color=Gray,
                                 fontSize=16.sp,
                             )
                             Spacer(modifier=Modifier.height(20.dp))
 
                             //Email
-                            Row(verticalAlignment = Alignment.CenterVertically){
-                                Icon(
-                                    imageVector= Icons.Default.Email,
-                                    contentDescription="email"
-                                )
-                                Spacer(modifier = Modifier.width(10.dp))
-                                Text(
-                                    text="Email",
-                                    fontSize = 14.sp,
-                                    fontWeight = FontWeight.Bold,
-                                )
-                            }
+                            Label(icon = Icons.Default.Email, text = "email")
+                            
                             Spacer(modifier=Modifier.height(10.dp))
                             Text(
                                 text = "example@example.com",
@@ -124,12 +119,49 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text(text = "詳細を表示",color=Color.White)
                         }
+                        Spacer(modifier = Modifier.height(20.dp))
+                        
+                        //趣味＆居住地
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray.copy(alpha = 0.3f))
+                                .padding(horizontal = 10.dp, vertical = 20.dp)
+                        ) {
+                            Label(
+                                icon = Icons.Default.Favorite,
+                                text = "趣味：個人開発",
+                                color = Gray,
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Label(
+                                icon = Icons.Default.LocationOn,
+                                text = "居住地：東京都大田区",
+                                color = Gray,
+                            )
+                        }
                     }
                 }
             }
         }
     }
+}
 
-
+@Composable
+fun Label(icon: ImageVector, text: String, color: Color= MaterialTheme.colors.onBackground){
+    Row(verticalAlignment = Alignment.CenterVertically){
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = text,
+            color = color,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
 
